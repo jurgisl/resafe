ba9b8483185bee3d795d1458ad516b158746b014
Jurgis Lukss <jurgis.lukss@luxtechnology.lv>
2012-11-28 10:57:24 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resafe::Application.config.secret_token = '46e51485598d91fb9a68754aeb6a140dbcaf20dad4530045d85110b535682ebe42b1beff8d037ed77160caea502b89e043adc64a0fff57656beda4aa07c434e4'
